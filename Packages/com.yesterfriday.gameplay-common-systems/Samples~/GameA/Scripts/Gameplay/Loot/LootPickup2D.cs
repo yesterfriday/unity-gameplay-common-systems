a007f389ee0c0dff2e1224baf482f38ca463738c
yesterfriday <qwe50922327@gmail.com>
2026-02-19 15:34:40 +0900

    feat(gamea): add importable GameA sample scripts and register sample

---
+++ Packages/com.yesterfriday.gameplay-common-systems/Samples~/GameA/Scripts/Gameplay/Loot/LootPickup2D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Yesterfriday.GameplayCommonSystems.SamplesCommon.Gameplay.Loot
+{
+    [RequireComponent(typeof(Collider2D))]
+    public sealed class LootPickup2D : MonoBehaviour
+    {
+        [SerializeField] private LootStack _loot;
+        [SerializeField] private LayerMask _playerLayerMask;
+
+        public event Action<LootStack> LootPicked;
+
+        private bool _picked;
+
+        public LootStack Loot => _loot;
+
+        public void SetLoot(LootStack loot) => _loot = loot;
+
+        private void Reset()
+        {
+            var col = GetComponent<Collider2D>();
+            col.isTrigger = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_picked)
+                return;
+
+            if (((1 << other.gameObject.layer) & _playerLayerMask.value) == 0)
+                return;
+
+            _picked = true;
+            LootPicked?.Invoke(_loot);
+            Destroy(gameObject);
+        }
+    }
+}
